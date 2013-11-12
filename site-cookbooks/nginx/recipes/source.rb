2e44d66fdc057f46ca4470f698101a519f873726
toritori0318 <toritori0318@gmail.com>
2013-11-13 02:07:07 +0900

    ngx_lua_module をincludeするように直した

--- site-cookbooks/nginx/recipes/source.rb
+++ site-cookbooks/nginx/recipes/source.rb
@@ -69,35 +69,9 @@ node.run_state['nginx_configure_flags'] =
 
 
 
-# ①ngx_lua_module.rb をコピペ
+# ①ngx_lua_module.rb をinclude_recipe
 ##############################
-lua_src_filename = ::File.basename(node['nginx']['lua']['url'])
-lua_src_filepath = "#{Chef::Config['file_cache_path']}/#{lua_src_filename}"
-lua_extract_path = "#{Chef::Config['file_cache_path']}/nginx-lua-#{node['nginx']['lua']['version']}"
-
-remote_file lua_src_filepath do
-  source   node['nginx']['lua']['url']
-  checksum node['nginx']['lua']['checksum']
-  owner    'root'
-  group    'root'
-  mode     '0644'
-end
-
-bash 'extract_lua_module' do
-  cwd  ::File.dirname(lua_src_filepath)
-  code <<-EOH
-    mkdir -p #{lua_extract_path}
-    tar xzf #{lua_src_filename} -C #{lua_extract_path}
-  EOH
-  not_if { ::File.exists?(lua_extract_path) }
-end
-
-node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--add-module=#{lua_extract_path}/lua-nginx-module-#{node['nginx']['lua']['version']}"]
-
-include_recipe 'nginx::lua'
-include_recipe 'nginx::ngx_devel_module'
-
+include_recipe 'nginx::ngx_lua_module'
 
 # ②lua cjson インストール
 ##############################
